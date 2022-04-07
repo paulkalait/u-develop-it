34890ff7370d1fcfb5c9d1f7aef2cf0cd8f8ba4f
Your Name <kalaitzidispaul@gmail.com>
2022-04-06 20:00:28 -0400

    added query request for rows

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2')
 const express = require("express")
 
 const PORT = process.env.PORT || 3001;
@@ -6,6 +7,23 @@ const app = express();
 //Express middleware
 app.use(express.urlencoded({extended: false}))
 app.use(express.json());
+
+//connect to sql database
+const db = mysql.createConnection(
+{
+    host: 'localhost',
+    //Your MySql username,
+    user: 'root',
+    //your mySQL Password
+    password: '',
+    database: 'election'
+},
+console.log('Connected to the election databse')
+)
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows)
+})
 
 //Default response for any other request (Not Found)
 app.use((req, res) => {
